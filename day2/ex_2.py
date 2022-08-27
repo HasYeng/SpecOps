f15856997a4b06b4b3c2215b12c2941fff7f52df
HasYeng <107547851+HasYeng@users.noreply.github.com>
2022-08-28 01:46:59 +0400

    Update ex_2.py

--- day2/ex_2.py
+++ day2/ex_2.py
@@ -1,8 +1,10 @@
-num = 0
-sum1 = 0
-while num < 1000:
-    if num % 3 == 0 or num % 5 == 0:
-        sum1 += num
-    num += 1
-print(sum1)
+def dev_by_3_5(num):
+    sum1 = 0
+    while num:
+        if num % 3 == 0 or num % 5 == 0:
+            sum1 += num
+        num -= 1
+    return sum1
 
+
+print(dev_by_3_5(999))
